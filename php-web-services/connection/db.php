9ea8858c42649a24a63c38ead1ed8e6792deeb2e
Adham Hayukalbu <adham@idealisconsulting.com>
2018-07-12 13:12:58 +0700

    [ADD] - Added Web Services UAS - Initial v1

---
+++ php-web-services/connection/db.php
@@ -0,0 +1,5 @@
+<?php
+    header("Access-Control-Allow-Origin: *");
+    // $con = mysqli_connect("localhost", "id6360184_root", "@hayukalbu", "id6360184_phonegap_demo") or die ("could not connect database");
+    $con = mysqli_connect("localhost", "root", "", "phonegap_demo") or die ("could not connect database");
+?>
